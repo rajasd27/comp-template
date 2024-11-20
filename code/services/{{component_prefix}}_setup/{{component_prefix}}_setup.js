7b4883773c84df181c36e58cca8f195f5f35128c
Rajas Deshpande <rajasdeshpande27@gmail.com>
2024-11-20 06:53:14 -0600

    init commit

---
+++ code/services/{{component_prefix}}_setup/{{component_prefix}}_setup.js
@@ -0,0 +1,19 @@
+/**
+ * Type: Micro Service
+ * Description: A short-lived service which is expected to complete within a fixed period of time.
+ * @param {CbServer.BasicReq} req
+ * @param {string} req.systemKey
+ * @param {string} req.systemSecret
+ * @param {string} req.userEmail
+ * @param {string} req.userid
+ * @param {string} req.userToken
+ * @param {boolean} req.isLogging
+ * @param {[id: string]} req.params
+ * @param {CbServer.Resp} resp
+ */
+
+function {{component_prefix}}_setup(req, resp) {
+  const params = req.params;
+  //component setup behavior here initialize any external databases, bucket sets, etc.
+  resp.success('Success');
+}
